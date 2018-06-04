c3734be2233c7f726534aaee2075347eb952fa80
Bahram Aghaei <aghaee.bahram@gmail.com>
2018-06-04 11:28:47 +0430

    Add Models for person and payment

--- counting/__init__.py
+++ counting/__init__.py
@@ -4,6 +4,8 @@
 # third-part imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
 # local imports
 from config import app_config
@@ -11,9 +13,17 @@ from config import app_config
 # db variable initialization
 db = SQLAlchemy()
 
+# login_manager variable initialization
+login_manager = LoginManager()
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
     db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.message = "You must be logged in to access this page"
+
+    migrate = Migrate(app, db)
+    from counting import models
 
     return app
--- counting/models.py
+++ counting/models.py
@@ -1,27 +1,66 @@
 #!/usr/bin/env python3
 # Author: GreatBahram
-from .app import db
+
+# third-party imports
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+
+# local imports
+from counting import db, login_manager
 
 class Person(db.Model):
     """
         Create a Person table
     """
 
-    __tablename__ == 'persons'
+    __tablename__ = 'persons'
 
-    id = db.Column(db.Integer, primary_key=True, autincrement=True)
-    forename = db.Column(db.String(60), nullable=False) 
-    surname = db.Column(db.String(60), nullable=False) 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(60), index=True, unique=True)
+    email = db.Column(db.String(60), index=True, unique=True)
+    forename = db.Column(db.String(60), nullable=False, index=True) 
+    surname = db.Column(db.String(60), nullable=False, index=True) 
+    password_hash = db.Column(db.String(128))
+    is_admin = db.Column(db.Boolean, default=False)
+
+    @property
+    def password(self):
+        """
+        Prevent password from being accessed.
+
+        """
+        raise AttributeError("Password is not readable attribute.")
+
+    @password.setter
+    def password(self, password):
+        """
+        Set password to a hashed password.
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """
+        Checked if hashed password matches actual password
+        """
+        return check_password_hash(self.password_hash, password)
 
     def to_dict(self):
         return {
                 'id': self.id,
                 'forname': self.forname,
                 'surname': self.surname,
+                'username': self.username,
+                'email': self.email,
                 }
+
     def __repr__(self):
         return "<Person: {} {}>".format(self.forename, self.surname)
+
+# setup user_loader
+@login_manager.user_loader
+def load_user(user_id):
+    return Person.query.get(int(user_id))
 
 
 class Payment(db.Model):
@@ -29,12 +68,12 @@ class Payment(db.Model):
         Create a Payment table
     """
 
-    __tablename__ == 'payments'
+    __tablename__ = 'payments'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    date = db.Column(db.Datetime, nullable=False)
-    buyer = db.Column(db.Integer, db.ForeinKey('persons.id'))
-    debtor = db.Column(db.Integer, db.ForeinKey('persons.id'))
+    #date = db.Column(db.Datetime, nullable=False)
+    buyer = db.Column(db.Integer, db.ForeignKey('persons.id'))
+    debtor = db.Column(db.Integer, db.ForeignKey('persons.id'))
     price = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(200))
     is_sale = db.Column(db.Boolean, default=False)

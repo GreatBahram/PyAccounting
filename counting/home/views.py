0534afa9601f726ead91f5a006906ba8734d8c98
Bahram Aghaei <aghaee.bahram@gmail.com>
2018-06-06 18:20:15 +0430

    Add admin dashborad to home blueprint

--- counting/home/views.py
+++ counting/home/views.py
@@ -1,9 +1,10 @@
 # third-party imports
-from flask import render_template
-from flask_login import login_required
+from flask import abort, render_template
+from flask_login import current_user, login_required
 
 # local imports
-from  . import home
+from . import home
+
 
 @home.route('/')
 def homepage():
@@ -19,3 +20,14 @@ def dashboard():
     Render the dashboard template on the /dashboard route
     """
     return render_template('home/dashboard.html', title="Dashboard")
+
+@home.route('/admin/dashboard')
+@login_required
+def admin_dashboard():
+    """
+    Render the dashboard template on the /admin/dashboard route
+    """
+    if not current_user.is_admin:
+        abort(403)
+
+    return render_template('home/admin_dashboard.html', title="Admin Dashboard")

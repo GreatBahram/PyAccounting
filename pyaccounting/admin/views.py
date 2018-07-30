567fa15a9fd89a7812fa824aaa2621d69c3dedbd
GreatBahram <aghaee.bahram@gmail.com>
2018-07-30 12:53:06 +0430

    add check_admin function

--- pyaccounting/admin/views.py
+++ pyaccounting/admin/views.py
@@ -1,1 +1,10 @@
+# third-party imports
+from flask import abort
+from flask_login import current_user
 
+def check_admin():
+    """
+    Prevent non-admins from accessing the page
+    """
+    if not current_user.is_admin:
+        abort(403)
